84b943ca919a2b1f5c2faf0ea81b8bc0d0b0eeff
sairam9396 <35089846+sairam9396@users.noreply.github.com>
2018-04-26 01:16:23 +0530

    Add files via upload

---
+++ StackOverflow/src/Client.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sai Bros
+ */
+
+    public class Client {  
+static void p(){  
+System.out.println("hello");  
+p();  
+}  
+  
+public static void main(String[] args) {  
+p();  
+}  
+}  
+    
+
